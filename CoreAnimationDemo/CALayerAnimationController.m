9fa7e73549ad1fb2ae87d022084d29b07440d6ac
dingfude <dingfude@dingfudedeMacBook-Pro.local>
2017-02-15 23:17:14 +0800

    隐式动画的启用和禁用，给UIView 添加自己的图层行为

--- CoreAnimationDemo/CALayerAnimationController.m
+++ CoreAnimationDemo/CALayerAnimationController.m
@@ -21,11 +21,13 @@
     [super viewDidLoad];
     
     //create sublayer
-    self.colorLayer = [CALayer layer];
-    self.colorLayer.frame = CGRectMake(50.0f, 50.0f, 100.0f, 100.0f);
-    self.colorLayer.backgroundColor = [UIColor blueColor].CGColor;
-    //add it to our view
-    [self.layerView.layer addSublayer:self.colorLayer];
+//    self.colorLayer = [CALayer layer];
+//    self.colorLayer.frame = CGRectMake(50.0f, 50.0f, 100.0f, 100.0f);
+//    self.colorLayer.backgroundColor = [UIColor blueColor].CGColor;
+//    //add it to our view
+//    [self.layerView.layer addSublayer:self.colorLayer];
+//    
+//    
     
     
     
@@ -43,21 +45,35 @@
      */
     
     
+    //create sublayer
+    self.colorLayer = [CALayer layer];
+    self.colorLayer.frame = CGRectMake(50.0f, 50.0f, 100.0f, 100.0f);
+    self.colorLayer.backgroundColor = [UIColor blueColor].CGColor;
+    //add a custom action
+    // 实现一个手动的图层效果
+    
+    CATransition *transition = [CATransition animation];
+    transition.type = kCATransitionPush;
+    transition.subtype = kCATransitionFromLeft;
+    self.colorLayer.actions = @{@"backgroundColor": transition};
+    //add it to our view
+    [self.layerView.layer addSublayer:self.colorLayer];
+    
 }
 - (IBAction)changeColor {
     
     // 在这里修改，会对中间的事务都起效果
-    [CATransaction begin];
-    
-    [CATransaction setAnimationDuration:1.0];
-    
-    //randomize the layer background color
-    CGFloat red = arc4random() / (CGFloat)INT_MAX;
-    CGFloat green = arc4random() / (CGFloat)INT_MAX;
-    CGFloat blue = arc4random() / (CGFloat)INT_MAX;
-    self.colorLayer.backgroundColor = [UIColor colorWithRed:red green:green blue:blue alpha:1.0].CGColor;
-
-    [CATransaction commit];
+//    [CATransaction begin];
+//    
+//    [CATransaction setAnimationDuration:1.0];
+//    
+//    //randomize the layer background color
+//    CGFloat red = arc4random() / (CGFloat)INT_MAX;
+//    CGFloat green = arc4random() / (CGFloat)INT_MAX;
+//    CGFloat blue = arc4random() / (CGFloat)INT_MAX;
+//    self.colorLayer.backgroundColor = [UIColor colorWithRed:red green:green blue:blue alpha:1.0].CGColor;
+//
+//    [CATransaction commit];
     
     
     // 类似的方法
@@ -68,6 +84,49 @@
     
     // 这个也是同样的
     // UIView animateWithDuration:<#(NSTimeInterval)#> animations:<#^(void)animations#>
+    
+    
+    // 完成块
+    
+    //begin a new transaction
+    [CATransaction begin];
+    //set the animation duration to 1 second
+    [CATransaction setAnimationDuration:1.0];
+    //add the spin animation on completion
+    [CATransaction setCompletionBlock:^{
+            //rotate the layer 90 degrees
+            CGAffineTransform transform = self.colorLayer.affineTransform;
+            transform = CGAffineTransformRotate(transform, M_PI_2);
+            self.colorLayer.affineTransform = transform;
+        }];
+    //randomize the layer background color
+    CGFloat red = arc4random() / (CGFloat)INT_MAX;
+    CGFloat green = arc4random() / (CGFloat)INT_MAX;
+    CGFloat blue = arc4random() / (CGFloat)INT_MAX;
+    self.colorLayer.backgroundColor = [UIColor colorWithRed:red green:green blue:blue alpha:1.0].CGColor;
+    //commit the transaction
+    [CATransaction commit];
+    
+    
+    // 新的API
+    // UIView animateWithDuration:<#(NSTimeInterval)#> animations:<#^(void)animations#> completion:<#^(BOOL finished)completion#>
+    
+    
+    
+    // 图层行为
+    
+    // 如果直接对视图的属性进行修改，会发现，及时添加了事务，但是仍然没有动画，
+    // 我们知道Core Animation通常对CALayer的所有属性（可动画的属性）做动画”
+    //但是UIView把它关联的图层的这个特性关闭了”
+    
+    /*
+        原因，每一个UIView 都扮演着起关联Layer 的委托对象，并且实现了一个-actionForLayer:forKey的方法，并对这个方法中应该返回的图层行为返回为nil, 就禁用了动画。 但是在 UIView的block范围内，就返回的是一个非空值，就实现了动画
+     
+     */
+    
+    
+    
+
     
 }
 
