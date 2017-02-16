64d91ddf9a2f877c4689beddffb21d5bf6944de4
dingfude <dingfude@dingfudedeMacBook-Pro-2.local>
2017-02-16 13:13:35 +0800

    【Update】改变原来的引入外部文件，改为copy

---
+++ CoreAnimationDemo/NSDate+Addition.h
@@ -0,0 +1,37 @@
+//
+//  NSDate+Addition.h
+//  FriedRice
+//
+//  Created by DFD on 2017/2/9.
+//  Copyright © 2017年 DFD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern const NSInteger SECOND;
+extern const NSInteger MINUTE;
+extern const NSInteger HOUR;
+extern const NSInteger DAY;
+extern const NSInteger MONTH;
+extern const NSInteger YEAR;
+
+@interface NSDate(Addition)
+
+@property (nonatomic, readonly) NSInteger	year;
+@property (nonatomic, readonly) NSInteger	month;
+@property (nonatomic, readonly) NSInteger	day;
+@property (nonatomic, readonly) NSInteger	hour;
+@property (nonatomic, readonly) NSInteger	minute;
+@property (nonatomic, readonly) NSInteger	second;
+@property (nonatomic, readonly) NSInteger	weekday;
+
+- (NSString *)stringWithDateFormat:(NSString *)format;
+- (NSString *)timeAgo;
+- (NSString *)timeLeft;
+
++ (long long)timeStamp;
+
++ (NSDate *)dateWithString:(NSString *)string format:(NSString*)format;
++ (NSDate *)now;
+
+@end
