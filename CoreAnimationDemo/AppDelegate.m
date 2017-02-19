6b2c8d094d84c35d8eab8523fa7393a52a8886b5
dingfude <dingfude@dingfudedeMacBook-Pro.local>
2017-02-19 22:36:57 +0800

    两种图片加载方式的对比

--- CoreAnimationDemo/AppDelegate.m
+++ CoreAnimationDemo/AppDelegate.m
@@ -34,6 +34,81 @@
 }
 
 
+/*
+    加载图片的两种方式的内存使用对比 Resourse（ +imageWithContentsOfFile:）与 imageAssets( +imageNamed:)
+ 
+    1, Resourse ( +imageWithContentsOfFile:)
+        优点：使用时载入内存，不用的时候自动销毁，内存管理及时。
+        适用：不经常使用到的大图，如果开机引导页
+     // 1, Resourse
+     /*
+     
+     NSString *path = [[NSBundle mainBundle] pathForResource:@"image@2x" ofType:@"pgn"];
+     UIImage *image = [UIImage imageWithContentsOfFile:path];
+     
+     内部实现逻辑；
+     
+     + (instancetype)imageWithContentOfFile:(NSString *)fileName{
+     
+     NSUInger scale = 0;
+     
+     //...
+     
+     scale = 2; // 使用屏幕的缩放比例，在 plus 上使用 3
+     
+     return [[self alloc] initWithData:[NSData dataWithContentOfFile:fileName scale:scale];
+     }
+     
+     // 这种加载图片的方式局限性是，图片必须在.ipa的根目录或者沙盒中。
+     // 2, imageAssets
+     
+     /*
+     使用的是图片缓存，保存在一个图片缓存字典中，key:图片名，value:UIImage.一旦加载过一次，就永不销毁，
+     
+     内部代码实现类似
+     + (NSMutableDictionary *)imageBuff{
+        static NSMutableDictionary *_imageBuff;
+        static dispatch_once_t onceToken;
+        dispatch_once(&onceToken, ^{
+            _imageBuff = [[NSMutableDictionary alloc] init];
+        });
+        return _imageBuff;
+     }
+     
+     + (instancetype)imageName:(NSString *)imageName{
+        if (!imageName) {
+            return nil;
+        }
+        UIImage *image = self.imageBuff[imageName];
+        if (image) {
+            return image;
+        }
+        NSString *path = @"这个是图片的路径";// 这里省略逻辑
+        if (image) {
+            self.imageBuff[imageName] = image;
+        }
+        return  image;
+     }
+
+        使用 icon类的图片， 大小一般在 3-20KB之间
+        优点：当 icon在多个地方显示的时候，只会创建一次，公用一个UIImage对象，减少对沙盒的读写操作。
+        缺点：图片缓存和APP生命周期同步
+ 
+     */
+
+- (void)setImage{
+
+    
+    
+    
+    
+    
+    
+}
+
+
+
+
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
